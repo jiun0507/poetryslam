435ac874d10c1e5716f19fb16a3d87e4be7689cb
Jiun Kim <jkim2@bowdoin.edu>
2020-11-17 00:50:46 +0900

    more

--- start.py
+++ start.py
@@ -1,13 +1,7 @@
-from random import randint
+from random import randint, randrange
 
 import pyrhyme
 
-g = pyrhyme.RhymeBrain()
-results = [x for x in g.rhyming('foo')]
-
-
-
-sentence_structures = {""}
 
 
 class RandomWordGenerator:
@@ -17,6 +11,7 @@ class RandomWordGenerator:
         self.adjectives = {}
         self.adverbs = {}
         self.subjects = ["I", "He", "She", "They", "We", "You"]
+        self.rhymer = pyrhyme.RhymeBrain()
 
         for i in range(1, 5):
             f = open(f"adjectives/{i}syllableadjectives.txt", "r")
@@ -35,6 +30,14 @@ class RandomWordGenerator:
             self.verbs[i] = f.read().split()
             f.close()
 
+    def make_rhyme(self, word):
+
+        rhymes = [x.word for x in self.rhymer.rhyming(word, lang='en')]
+        if len(rhymes) == 0:
+            return word
+        else:
+            return rhymes[0]
+
     def print_sample(self):
 
         for i in range(1, 5):
@@ -42,15 +45,16 @@ class RandomWordGenerator:
 
     def generate_word(self, word):
         if word == "Noun":
-            return self.random_noun():
-        if word == "Adjective":
-            return self.random_adjective():
-        if word == "Adverb":
-            return self.random_adverb():
-        if word == "Verb":
-            return self.random_verb():
-        if word == "Subject":
-            return self.random_subject():
+            return self.random_noun()
+        elif word == "Adjective":
+            return self.random_adjective()
+        elif word == "Adverb":
+            return self.random_adverb()
+        elif word == "Verb":
+            return self.random_verb()
+        elif word == "Subject":
+            result = self.random_subject()
+            return result
 
 
     def random_subject(self):
@@ -58,19 +62,19 @@ class RandomWordGenerator:
 
     def random_verb(self):
         syllable = randint(1,4)
-        return self.verbs[syllable][randint(0, len(self.verbs)-1)]
+        return self.verbs[syllable][randrange(0, len(self.verbs[syllable])-1)]
 
     def random_adjective(self):
         syllable = randint(1,4)
-        return self.adjectives[syllable][randint(0, len(self.adjectives)-1)]
+        return self.adjectives[syllable][randrange(0, len(self.adjectives[syllable])-1)]
 
     def random_adverb(self):
         syllable = randint(1,4)
-        return self.adverbs[syllable][randint(0, len(self.adverbs)-1)]
+        return self.adverbs[syllable][randrange(0, len(self.adverbs[syllable])-1)]
 
     def random_noun(self):
         syllable = randint(1,4)
-        return self.nouns[syllable][randint(0, len(self.nouns)-1)]
+        return self.nouns[syllable][randrange(0, len(self.nouns[syllable])-1)]
 
 class RandomSentenceGenerator:
     def __init__(self):
@@ -78,17 +82,33 @@ class RandomSentenceGenerator:
         self.sentence_structures = {
             0: ["Subject", "Verb"],
             1: ["Subject", "Verb", "Noun"],
-            2: ["Subject", "Verb", "adjective"],
-            3: ["Subject", "Verb", "adverb"],
+            2: ["Subject", "Verb", "Adjective"],
+            3: ["Subject", "Verb", "Adverb"],
             4: ["Noun", "Verb", "Noun"],
         }
 
     def generate_sentence(self):
         sentence_structure = self.sentence_structures[randint(0, 4)]
-        result = ""
-        for word in sentence_structures:
-            result += self.word_generator(word)
-            result += " "
+        first_line = ""
+        second_line = ""
+        rhyme_index = randint(0, len(sentence_structure)-1)
+        for index, word in enumerate(sentence_structure):
+            new_word = self.word_generator.generate_word(word)
+            new_word2 = self.word_generator.generate_word(word)
+            if index == rhyme_index:
+                new_word2 = self.word_generator.make_rhyme(new_word)
+            first_line += new_word + " "
+            second_line += new_word2 + " "
+        return first_line + "\n" + second_line
 
-        return result[-1:]
 
+sentence_generator = RandomSentenceGenerator()
+
+poem = ""
+for i in range(1, 4):
+    sentence = sentence_generator.generate_sentence()
+    poem += sentence + "\n"
+
+import os
+
+os.system(f"say -v Alex '{poem}'")
